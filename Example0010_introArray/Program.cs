8d57ac21ca47d870191786fdcadd5157aeb8c76e
Maksim Akimov <maksimakimov.96@mail.ru>
2023-03-26 21:23:42 +0300

    add Example0010

---
+++ Example0010_introArray/Program.cs
@@ -0,0 +1,16 @@
+﻿// Нахождение наибольшего числа из 9, с помощью функции/метода.
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {11,42,13,24,85,46,37,78,99};
+int result = Max
+            (Max(array[0], array[1], array[2]),
+             Max(array[3], array[4], array [5]),
+             Max(array[6], array[7], array[8])
+ );
+Console.WriteLine(result);

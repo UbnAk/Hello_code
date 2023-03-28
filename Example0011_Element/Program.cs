e8b27c9f250e6fe31bf0defed3b00f13d5ed2705
Maksim Akimov <maksimakimov.96@mail.ru>
2023-03-28 18:50:21 +0300

    New

--- Example0011_Element/Program.cs
+++ Example0011_Element/Program.cs
@@ -5,18 +5,33 @@
 // 3. Увеличить index на 1.
 // 4. Если index < n, то перейти к шагу 2. в противном случае алгорртим завершит работу успешно.
 
-int[] array = {23, 45, 90, 82, 76, 87, 34, 73, 82};
+int[] array = {23, 45, 90, 82, 76, 87, 34, 73, 82}; //задали массив
 
-int n = array.Length;
-int find = 82;
-int index = 0;
+int n = array.Length; // n элементы, Length-длина массива array
+int find = 82; // find просто решили взять число 82
+int index = 0; // изначально index = 0;
 
-while (index<n)
+while (index<n) // делаем цикл. Пока индекс меньше длины массива
 {
-    if(array[index]== find)
+    if(array[index]== find) // Если array[0] == 82, то выводим ниже, если нет то индекс +1
     {
         Console.WriteLine(index);
         break;   // если в массиве будет два однаковых числа, найдя первый он прерветься.
     }
     index++;
 }
+
+// int[] Massiv = {234,124,765,22,14,665,124,66423,123,13};
+// int n = Massiv.Length;
+// int index = 0;
+// int find = 123;
+
+// while(index < n)
+// {
+//     if(Massiv[index] == find)
+//     {
+//         System.Console.WriteLine(index);
+//         break;
+//     }
+//     index++;
+// }
